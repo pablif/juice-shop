5df5b8a75413c703a1d743fddf0c1ec3bf26f487
pablif <nhz01530@educastur.es>
2024-12-12 18:30:26 +0100

    Update profileImageUrlUpload.ts

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -15,10 +15,11 @@ const request = require('request')
 module.exports = function profileImageUrlUpload () {
   return (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
-      const url = req.body.imageUrl
+      const url =new URL( req.body.imageUrl);
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
+        if (url.hostname == 'localhost'){
         const imageRequest = request
           .get(url)
           .on('error', function (err: unknown) {
