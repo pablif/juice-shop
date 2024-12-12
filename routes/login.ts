f4ea989bb926f75f772c0c59ad3b6d2a455fc0eb
pablif <nhz01530@educastur.es>
2024-12-12 18:07:56 +0100

    Update login.ts

--- routes/login.ts
+++ routes/login.ts
@@ -33,8 +33,13 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-      .then((authenticatedUser) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    const User = models.sequelize.define("User",
+     (email:req.body.email ),
+     (password:req.body.password ),
+     (deletedAt: )
+       ));
+
+    User.findOne() // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
           res.status(401).json({
